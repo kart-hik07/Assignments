66128d62467a8d13f9e0cee45e04d71ac9018da0
kart-hik07 <99004062+kart-hik07@users.noreply.github.com>
2024-08-10 12:24:25 +0530

    Update lin_reg.py

--- btech/python/lin_reg.py
+++ btech/python/lin_reg.py
@@ -1,32 +1,29 @@
-#polynomial regression
 
-#importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-#Importing the dataset
+
 dataset = pd.read_csv('input_output_fitting.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-#splitting the data into the training set and test set
+
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)""" 
 
-#feature scaling
+
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""                                                    
 
-# Fitting linear regresssion to the dataset
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
-#Fitting Polynomial Regression to the dataset
+
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 3) # ax3 + bx2 + cx + d
 X_poly = poly_reg.fit_transform(X)
@@ -34,7 +31,6 @@ X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-#Visualizing the polynomial regression results
 plt.scatter(X, y, color= 'red')
 plt.plot(X, lin_reg_2.predict(X_poly), color = 'blue')
 plt.title('Polynomial Regression')
@@ -48,7 +44,7 @@ root = tk.Tk()
 
 canvas1 = tk.Canvas(root, width = 500, height =300)
 canvas1.pack()
-#New_interest_rate label and input box
+
 label1 = tk.Label(root, test='Enter Input: ')
 canvas1.create_window(100, 50, window=label1)
 
